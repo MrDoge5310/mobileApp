70c89fb67838fce55af03ec2cb833460fb3c2c69
LOGIKA <danilltrotsenko2002@gmail.com>
2024-05-18 13:08:29 +0300

    1

--- main.py
+++ main.py
@@ -2,26 +2,43 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+class MainScreen(Screen):
+    def __init__(self, name='main'):
+        super().__init__(name=name)
+
+
+class HeartBeatCheckScreen(Screen):
+    def __init__(self, name='heart_beat_check'):
+        super().__init__(name="heart_beat_check")
+
+
+class SeatSetScreen(Screen):
+    def __init__(self, name="seat_set"):
+        super().__init__(name="seat_set")
+
+
+class RestScreen(Screen):
+    def __init__(self, name="rest"):
+        super().__init__(name="rest")
+
+
+class ResultsScreen(Screen):
+    def __init__(self, name="results"):
+        super().__init__(name="results")
 
 
 class MyApp(App):
     def build(self):
-        label1 = Label(text="Hello!")
-        button1 = Button(text="Click me!")
-        button2 = Button(text="Click me!")
-        button3 = Button(text="Click me!")
-        button4 = Button(text="Click me!")
-
-        h1 = BoxLayout(orientation="horizontal", padding=10)
-        h1.add_widget(button1)
-        h1.add_widget(button2)
-        h1.add_widget(button3)
-        h1.add_widget(button4)
-
-        h2 = BoxLayout(orientation="vertical")
-        h2.add_widget(label1)
-        h2.add_widget(h1)
-        return h2
+        sm = ScreenManager()
+        sm.add_widget(MainScreen())
+        sm.add_widget(HeartBeatCheckScreen())
+        sm.add_widget(SeatSetScreen())
+        sm.add_widget(RestScreen())
+        sm.add_widget(ResultsScreen())
+        return sm
 
 
 app = MyApp()
